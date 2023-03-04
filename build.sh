88bf91bab80a5ec0bc4ee631de3533d9c4aac040
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-04 17:59:28 -0500

    ???? 	modified:   .cirrus.yml 	modified:   build.sh 	modified:   setup.sh

--- build.sh
+++ build.sh
@@ -4,44 +4,11 @@
 # Copyright (C) 2020-2021 Adithya R.
 # (edits for CrystalCore kernel @dkpost3)
 
-###############################   MISC   #################################
-
-# functions
-error() {
-	telegram-send "Error⚠️: $*"
-	exit 1
-}
-
-success() {
-	telegram-send "Success: $*"
-}
-
-inform() {
-	telegram-send --format html "$@"
-}
-
-muke() {
-	if [[ -z $COMPILER || -z $COMPILER32 ]]; then
-		error "Compiler is missing"
-	fi
-	if ! make $@ ${MAKE_ARGS[@]} $FLAG; then
-		error "make failed"
-	fi
-}
-
 ##----------------------------------------------------------##
 
-BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-BOT_BUILD_URL="https://api.telegram.org/bot$TOKEN/sendDocument"
-SECONDS=0 # builtin bash timer
-PROCS=$(nproc --all)
-CI="Cirrus CI"
-CHATID=${chat_id}
-TOKEN=${token}
-
-inform()
+tg_post_msg()
 {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$chat_id" \
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
@@ -66,12 +33,14 @@ tg_post_build()
 MODEL="Xiaomi 11 Lite 5G NE"
 DEVICE="lisa"
 ARCH=arm64
-KERNEL_DIR=$(pwd)
-TOOLCHAIN="$KERNEL_DIR/../toolchains"
-TC_DIR="$TOOLCHAIN/clang"
-export PATH="$TC_DIR/bin:$PATH"
-AK3_DIR="$KERNEL_DIR/../AnyKernel3"
-OUTPUT="$KERNEL_DIR/out"
+BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+SECONDS=0 # builtin bash timer
+PROCS=$(nproc --all)
+CI="Cirrus CI"
+CHATID="-1001293242785"
+TC_DIR="$CWk_DIR/clang"
+OUTPUT="$CWK_DIR/Kernel/out"
 DEFCONFIG="lisa_defconfig"
 KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 MAKE_PARAMS1="ARCH=arm64 CC=$TC_DIR/bin/clang CLANG_TRIPLE=$TC_DIR/bin/aarch64-linux-gnu- LD=$TC_DIR/bin/ld.lld LLVM=1 LLVM_IAS=1 \
@@ -91,7 +60,7 @@ export KBUILD_BUILD_USER ARCH SUBARCH PATH \
 export DISTRO=$(source /etc/os-release && echo "${NAME}")
 export KBUILD_BUILD_HOST=$(uname -a | awk '{print $2}')
 export CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-TERM=xterm-256color
+TERM=xterm
 
 ## Check for CI
 if [ "$CI" ]
@@ -110,11 +79,11 @@ then
 		export BASEDIR=$DRONE_REPO_NAME
 		export SERVER_URL="${DRONE_SYSTEM_PROTO}://${DRONE_SYSTEM_HOSTNAME}/${AUTHOR}/${BASEDIR}/${KBUILD_BUILD_VERSION}"
 	else
-		inform "##----------------------------------------------------------##"
+		tg_post_msg "<b>##----------------------------------------------------------##</b>"
 	fi
 fi
 
-BLDV="R0.2-v0.0.1"
+BLDV="R0.3-v0.0.0"
 ZIPNAME="Proton-$BLDV.zip"
 ZIPSIGNED="Proton-$BLDV-signed.zip"
 
@@ -125,20 +94,24 @@ export PATH="$TC_DIR/bin:$PATH"
 
 make $MAKE_PARAMS mrproper
 make $MAKE_PARAMS $DEFCONFIG
-cp "$OUTPUT"/.config $KERNEL_DIR/arch/arm64/configs/lisa_defconfig
-telegram-send --file "$KERNEL_DIR/out/.config"
-inform "<b>Successfully regenerated defconfig at $DEFCONFIG</b>"
+cp "$OUTPUT"/.config $KERNEL_SRC/arch/arm64/configs/lisa_defconfig
+tg_post_build "$KERNEL_SRC/out/.config"
+tg_post_msg "<b>Successfully regenerated defconfig at $DEFCONFIG</b>"
 
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 	rm -rf out
+	echo "Cleaned output folder"
 fi
+
+# New defconfig to use instead
+LISADEF="lisa_defconfig"
 
 mkdir -p out
 make $MAKE_PARAMS $DEFCONFIG
 
-inform "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KV</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$CI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<a href='$SERVER_URL'>Link</a>"
-inform "<b>Starting compilation</b>"
+tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KV</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$CI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<a href='$SERVER_URL'>Link</a>"
+tg_post_msg "<b>Starting compilation</b>"
 make -j$(nproc --all) $MAKE_PARAMS || exit $?
 make -j$(nproc --all) $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
 
@@ -147,11 +120,11 @@ dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
 dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
 
 if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
-	inform "<b>Kernel compiled succesfully!</b>"
+	tg_post_msg "<b>Kernel compiled succesfully!</b>"
 fi
 	cp $kernel $AK3_DIR
 	cp $dtb $AK3_DIR/dtb
-	inform "<b>Creating DTBO Image</b>"
+	tg_post_msg "<b>Creating DTBO Image</b>"
 	python3 scripts/dtc/libfdt/mkdtboimg.py create $AK3_DIR/dtbo.img --page_size=4096 $dtbo
 	cp $(find out/modules/lib/modules/5.4* -name '*.ko') $AK3_DIR/modules/vendor/lib/modules/
 	cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} $AK3_DIR/modules/vendor/lib/modules
@@ -159,11 +132,12 @@ fi
 	sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' $AK3_DIR/modules/vendor/lib/modules/modules.dep
 	sed -i 's/.*\///g' $AK3_DIR/modules/vendor/lib/modules/modules.load
 	rm -rf out/arch/arm64/boot out/modules
-	inform "<b>!Zipping Up!</b>"
+	tg_post_msg "<b>!Zipping Up!</b>"
 	cd $AK3_DIR
 	zip -r9 "$ZIPNAME" * -x ".git" -x ".github" -x "README.md" -x "*placeholder"
-	telegram-send --file "${ZIPNAME}"
-	inform "<b>!Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!</b>"
+	echo "Zip: $ZIPNAME"
+	tg_post_build "${ZIPNAME}"
+	tg_post_msg "<b>!Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!</b>"
 	cd ..
 	exit
 fi
--- setup.sh
+++ setup.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
-# Install pigz
-pacman -Sy --needed --noconfirm pigz
-
-pip3 -q install telegram-send
+KERNEL_SRC="$CWk_DIR/Kernel"
+AK3_DIR="$CWk_DIR/AnyKernel3"
 
 # Helper function for cloning: gsc = git shallow clone
 gsc() {
 	git clone --depth=1 -q $@
 }
 
-# Toolchains directory
-mkdir toolchains
-
-# Clone GCC
-gsc https://github.com/mvaisakh/gcc-arm.git -b gcc-master toolchains/gcc32
-
 # Clone Neutron Clang
-mkdir toolchains/clang && cd toolchains/clang
-bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=latest
+echo "Downloading Neutron Clang"
+mkdir $CWk_DIR/clang
+cd $CWk_DIR/clang
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=16012023
 cd ../..
 
-# Clone AnyKernel3
-gsc https://github.com/ghostrider-reborn/AnyKernel3.git -b lisa AnyKernel3
+echo "Cloning AnyKernel3"
+mkdir $AK3_DIR
+gsc https://github.com/ghostrider-reborn/AnyKernel3.git -b lisa $AK3_DIR
 
 # Clone Kernel Source
-gsc https://github.com/KazuDante89/android_kernel_ghost_lisa.git -b Proton_R0.3 Kernel
+mkdir $KERNEL_SRC
+echo "Downloading Kernel Source.."
+gsc https://github.com/KazuDante89/android_kernel_ghost_lisa.git -b Proton_R0.3 $KERNEL_SRC
 
-# Move script to kernel source
-mv build.sh Kernel/build.sh
-cd Kernel
-
-# Execute build script
-bash build.sh
+# Start build process
+cd $KERNEL_SRC
+bash <(curl -s https://raw.githubusercontent.com/KazuDante89/Cirrus-CI/main/build.sh)
