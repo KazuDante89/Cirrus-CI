2dc0a637e47379c3e919df1e986bde8c16cc4b2c
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-04 15:57:24 -0500

    test 4 	modified:   .cirrus.yml 	modified:   build.sh 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# Install pigz
+pacman -Sy --needed --noconfirm pigz
+
 # Helper function for cloning: gsc = git shallow clone
 gsc() {
 	git clone --depth=1 -q $@
 }
 
+# Toolchains directory
+mkdir toolchains
+
+# Clone GCC
+gsc https://github.com/mvaisakh/gcc-arm.git -b gcc-master toolchains/gcc32
+
 # Clone Neutron Clang
-mkdir clang
-cd clang
+mkdir toolchains/clang && cd toolchains/clang
 bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=16012023
 cd ../..
 
@@ -18,7 +26,7 @@ gsc https://github.com/ghostrider-reborn/AnyKernel3.git -b lisa AnyKernel3
 gsc https://github.com/KazuDante89/android_kernel_ghost_lisa.git -b Proton_R0.2 Kernel
 
 # Move script to kernel source
-mv build.sh Kernel/build.sh
+mv $(pwd)/build.sh Kernel/build.sh
 cd Kernel
 
 # Execute build script
